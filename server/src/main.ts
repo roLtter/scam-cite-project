cd66f77eee90f1ad9b699a938182616efbf6c235
roLtter <engdavelish@gmail.com>
2025-10-28 22:32:55 +0300

    minor back fix

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,10 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'scam-cite-project.vercel.app',
-    ],
+    origin: ['http://localhost:5173', 'https://scam-cite-project.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
