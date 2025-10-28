0b7bc1c86bde3361f549efe11f85af357f253f47
roLtter <engdavelish@gmail.com>
2025-10-28 22:12:42 +0300

    linking versal + render

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5173',
-      'https://твойпроект.vercel.app',
+      'scam-cite-project.vercel.app',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
